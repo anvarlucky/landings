9d1e50071d15187b4f30be80459be954758d6d48
anvarlucky <anvarmath@gmail.com>
2021-01-18 17:07:13 +0500

    1802

---
+++ database/migrations/2020_12_21_122534_create_peoples_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeoplesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peoples', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('position');
+            $table->string('images');
+            $table->text('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peoples');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route gruppa sozdat qilib olamiz
+
+Route::group(['middleware' => 'web'], function(){
+    Route::match(['get', 'post'],'/','IndexController@execute')->name('home');
+    Route::get('/page/{alias}','PageController@execute')->name('page');
+    Route::auth();
+});
+
+//adminka
+Route::group(['prefix'=>'admin','middleware' => 'auth'], function (){
+   Route::get('/', function(){
+
+   });
+
+   Route::group(['prefix'=>'pages'], function (){
+      Route::get('/','PageController@execute')->name('pages');
+       Route::match(['get', 'post'],'/add','PageAddController@execute')->name('pagesAdd');
+       Route::match(['get', 'post','delete'],'/edit/{page}','PageEditController@execute')->name('pagesEdit');
+   });
+
+    Route::group(['prefix'=>'portfolios'], function (){
+        Route::get('/','PortfolioController@execute')->name('portfolio');
+        Route::match(['get', 'post'],'/add','PortfolioAddController@execute')->name('portfolioAdd');
+        Route::match(['get', 'post','delete'],'/edit/{portfolio}','PortfolioEditController@execute')->name('portfolioEdit');
+    });
+
+    Route::group(['prefix'=>'services'], function (){
+        Route::get('/','ServiceController@execute')->name('services');
+        Route::match(['get', 'post'],'/add','ServiceAddController@execute')->name('servicesAdd');
+        Route::match(['get', 'post','delete'],'/edit/{services}','ServiceEditController@execute')->name('servicesEdit');
+    });
 });
